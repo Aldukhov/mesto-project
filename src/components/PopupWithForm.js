072343d236563ec2c9f2eacbfeb3167bb8536c4a
Татьяна Никулина <19nikulina@gmail.com>
2023-07-04 22:19:17 +0700

    добавлен новый класс

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,43 @@
+import Popup from "./POPUP";
+
+export default class PopupWithForm extends Popup {
+    constructor (methodApi) {
+        this._method = methodApi;
+        this._form = this._popup.querySelector('.popup__form');
+        this._inputArr = this._popup.querySelector('popup__input');
+        this._button = this._popup.querySelector('.popup__button');
+    }
+    _getInputValues() {
+        this._inputValuesList = {};
+        this._inputArr.forEach((input) => {
+            this._inputValuesList[input.name] = input.value;
+        });
+        return this._inputValuesList;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._renderLoading(true);
+            this._method(this._getInputValues())
+            .then(() => this.close())
+            .finally(() => {
+            this._renderLoading(false);
+            });
+        })
+    }
+
+    _renderLoading(isLoading) {
+        if(isLoading) {
+            this._button.textContent = 'Сохранение..';
+          } else {
+            this._button.textContent = 'Сохранить';
+          }
+    }
+
+    close() {
+        super.close();
+        this._form.reset();
+    }
+}
