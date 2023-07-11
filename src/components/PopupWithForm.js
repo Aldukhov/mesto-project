0d007d9c661212b1caf575c8958d8a7fc80c32e2
Татьяна Никулина <19nikulina@gmail.com>
2023-07-11 13:13:42 +0700

    исправлен рендер загрузки

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -21,7 +21,7 @@ export default class PopupWithForm extends Popup {
         super.setEventListeners();
         this._form.addEventListener('submit', (evt) => {
             evt.preventDefault();
-            this._renderLoading(true);
+            this.renderLoading(true);
             this._handleFormSubmit(this._getInputValues());
             
         })
